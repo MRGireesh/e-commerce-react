743db1decd54fe8c6e8597cc47275e741ab7e356
GireeshMR <84763693+GireeshMR@users.noreply.github.com>
2023-11-11 00:47:59 +0530

    feature-Navigationbuttons added

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,13 @@
 import { Slider } from "../Slider/Slider";
 import { Navbar } from "../Navbar/Navbar";
+import { NavigationButtons } from "../NavigationButtons/NavigationButtons";
 
 export const Main = () => {
   return (
     <div>
       <Navbar />
       <Slider/>
+      <NavigationButtons/>
     </div>
   );
 };
---
+++ src/Components/NavigationButtons/NavigationButtons.js
@@ -0,0 +1,34 @@
+import { Button } from "@material-tailwind/react";
+import clothes from '../../assets/images/clothes.jpg'; 
+
+export const NavigationButtons = () =>{
+    const buttons = [
+        "Hoodies",
+        "Dresses",
+        "Suits",
+        "Shoes",
+        "T-Shirts",
+        "Jeans",
+        "Jackets",
+        "Bags",
+      ];
+
+      return(
+        <div>
+            <div className="flex item-center justify-center py-8">
+                {
+                buttons.map((button, index)=>{
+                    return(
+                        <div key={index} className="mr-4"><Button color="gray" size="lg" variant="outlined" ripple={true} className="hove:bg-green-300 duration-300 ease-in-out">{button}</Button></div>
+                    );
+                })}
+            </div>
+            <div className="bg-green-300 p-2 w-[55%] mx-auto rounded">
+                <h3 className="text-white text-center text-lg font-inter font-bold tracking-normal leading-none">***DISCOUNT UPTO 50% OFF***</h3>
+            </div>
+            <div className="flex justify-center item-center py-4">
+                <img className="h-[600px] w-[70%] rounded-md shadow-lg shadow-gray-600" src={clothes} alt="clothes"/>
+            </div>
+        </div>
+      )
+}
